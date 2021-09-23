55c6055e6a2f418956a523e37c84fda139de255a
ekarazas <karazas7@gmail.com>
2021-09-23 11:28:54 +0300

    Created a form tha takes todo value and puts it in todos state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Form from "./components/Form/Form";
 
 const App = () => {
-  return <h1>Hello, React</h1>;
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  // Setting title value
+
+  const takeTitleValue = (e) => {
+    setTitle(e.target.value);
+  };
+
+  // SubmitHandling
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+
+    setTodos([
+      ...todos,
+      {
+        id: Math.random() * 1000,
+        title: title,
+        completed: false,
+      },
+    ]);
+
+    //Setting title input to empty
+    setTitle("");
+  };
+
+  return (
+    <>
+      <Header />
+      <Form
+        submitHandle={submitHandle}
+        takeTitleValue={takeTitleValue}
+        title={title}
+      />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Form = ({ submitHandle, takeTitleValue, title }) => {
+  return (
+    <div className="wrapper">
+      <form onSubmit={submitHandle}>
+        <input
+          value={title}
+          onChange={takeTitleValue}
+          type="text"
+          name="title"
+          className="todo-input"
+          placeholder="Task title..."
+        />
+        <div className="button-position">
+          <button type="submit" className="add-button">
+            Add task
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
